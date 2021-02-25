1abb4fcbb16af8b7a3e187e93e4cfe1824ad07a6
awebb-palletline <57364421+awebb-palletline@users.noreply.github.com>
2021-02-25 16:06:21 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Header_ingest'); // Create a table for 'Header_ingest' with default settings
+mobileApp.tables.add('Header'); // Create a table for 'Header' with default settings
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
